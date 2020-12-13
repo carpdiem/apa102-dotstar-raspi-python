50069e92aea3eae81f9ca4a947c72d797b97b8bd
Michael Woods <mail@michaelwoods.us>
2020-12-13 14:07:11 -0800

    added thermal limit info

--- apa102_dotstar_raspi.py
+++ apa102_dotstar_raspi.py
@@ -5,7 +5,7 @@ valid_arrangements = ['linear']
 valid_update_strategies = ['on-command']
 
 class DotstarDevice:
-    def __init__(self, num_LEDs, arrangement, color_order, max_Hz = 3000000, bus = 0, device = 0, update = 'on-command'):
+    def __init__(self, num_LEDs, arrangement, color_order, thermal_protection_limit = False, max_Hz = 3000000, bus = 0, device = 0, update = 'on-command'):
         self.num_LEDs = num_LEDs
         # Store LEDs state in (brightness, r, g, b) order
         self.LEDs_state = [(0, 0, 0, 0)] * self.num_LEDs
@@ -17,6 +17,12 @@ class DotstarDevice:
             raise ValueError("invalid color_order value")
         else:
             self.color_order = color_order.lower()
+        # note, thermal protection limit is in units of (brightness ∈ [0, 31]) * (color_channel ∈ [0, 255]),
+        #   and is enforced on each color channel separately
+        if thermal_protection_limit != False:
+            self.thermal_limit = thermal_protection_limit
+        else:
+            self.thermal_limit = 31 * 255
         self.max_Hz = float(max_Hz)
         self.device = int(device)
         self.bus = int(bus)
@@ -28,6 +34,15 @@ class DotstarDevice:
         self.spi.open(self.bus, self.device)
         self.spi.max_speed_hz = int(self.max_Hz)
 
+    def safe_change_LED_state(idx, brightness, r, g, b):
+        if brightness * r > self.thermal_limit:
+            r = int(self.thermal_limit / brightness)
+        if brightness * g > self.thermal_limit:
+            g = int(self.thermal_limit / brightness)
+        if brightness * b > self.thermal_limit:
+            b = int(self.thermal_limit / brightness)
+        self.LEDs_state[idx] = (brightness, r, g, b)
+
     def set_LEDs(self, start_idx, end_idx, brightness, r, g, b):
         if start_idx < 0 or start_idx > self.num_LEDs - 1:
             raise ValueError("invalid start index: " + str(start_idx))
@@ -38,10 +53,10 @@ class DotstarDevice:
         if r not in range(256) or g not in range(256) or b not in range(256):
             raise ValueError("invalid rgb tuple: (" + str(r) + ", " + str(g) + ", " + str(b) + ")")
         for i in range(start_idx, end_idx):
-            self.LEDs_state[i] = (brightness, r, g, b)
+            self.set_LED(i, brightness, r, g, b)
 
     def set_LED(self, idx, brightness, r, g, b):
-        self.set_LEDs(idx, idx+1, brightness, r, g, b)
+        self.safe_change_LED_state(idx, brightness, r, g, b)
 
     def set_nth_LEDs(self, start_idx, n, brightness, r, g, b):
         for i in range(start_idx, self.num_LEDs, n):
